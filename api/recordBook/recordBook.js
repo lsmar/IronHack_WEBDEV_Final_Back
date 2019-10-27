d660257ea1f4ea9b103e157ac2dc3dfe9069f1f5
LsM Lucas Martins <lucas.smar@gmail.com>
2019-10-27 15:37:35 -0300

    fix record Schema

--- api/recordBook/recordBook.js
+++ api/recordBook/recordBook.js
@@ -1,30 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const recordBookSchema = new mongoose.Schema({
-      project: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-      },
-      students: {
-        type: mongoose.Schema.Types.ObjectId,
-        required: true
-      },
-      description: String,
-      tags: {
-        type: [String],
+const recordBookSchema = new mongoose.Schema(
+  {
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    students: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true
+    },
+    description: String,
+    tags: [
+      {
+        type: String,
         //TODO: definir as tags padronizadas
-        enum: ['opcaoA', 'opcaoB'],
-        image: {
-          type: String,
-          //TODO: definir imagem default
-          default: "url_bonequinho"
-        },
+        enum: ["opcaoA", "opcaoB"],
         required: true
       }
+    ],
+
+    image: {
+      type: String,
+      //TODO: definir imagem default
+      default: "url_bonequinho"
+    }
   },
   {
     timestamps: true
   }
 );
 
-module.exports = mongoose.model('RecordBook', recordBookSchema);
+module.exports = mongoose.model("RecordBook", recordBookSchema);
