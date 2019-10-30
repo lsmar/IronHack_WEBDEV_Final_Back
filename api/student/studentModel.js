dd4aa5b83848dd0fb248275bdee0983d9b1e3fe6
LsM Lucas Martins <lucas.smar@gmail.com>
2019-10-30 12:12:41 -0300

    student img

--- api/student/studentModel.js
+++ api/student/studentModel.js
@@ -1,37 +1,40 @@
 const mongoose = require("mongoose");
 
-const studentSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
+const studentSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    project: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Project"
+    },
+    image: {
+      type: String,
+      //TODO: definir imagem default
+      default: "https://res.cloudinary.com/lsm/image/upload/v1572448108/lynx-students/perfil-icon.png.png"
+    },
+    //* Série
+    classRoom: {
+      type: String,
+      required: true
+    },
+    //* Turma
+    grade: {
+      type: String,
+      required: true
+    },
+    classNumber: Number,
+    institution: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Institution",
+      required: true
+    }
   },
-  project: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Project"
-  },
-  image: {
-    type: String,
-    //TODO: definir imagem default
-    default: "url_bonequinho"
-  },
-  //* Série
-  classRoom: {
-    type: String,
-    required: true
-  },
-  //* Turma
-  grade: {
-    type: String,
-    required: true
-  },
-  classNumber: Number,
-  institution: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Institution",
-    required:true
-  },
-}, {
-  timestamps: true
-});
+  {
+    timestamps: true
+  }
+);
 
 module.exports = mongoose.model("Student", studentSchema);
